fcd42411c4993bd09b94a5f044b15f069a62f307
gabrielmenezes30 <menezesg792@gmailcom>
2021-11-22 12:08:54 -0300

    layout dashboard

--- src/views/dashboard/Dashboard.ts
+++ src/views/dashboard/Dashboard.ts
@@ -1,5 +1,18 @@
-import { IonPage, IonContent, IonButtons, IonMenuButton, IonList, IonItem, menuController, IonThumbnail, IonCard, IonCardHeader, IonCardContent, IonButton } from '@ionic/vue';
+import { IonPage, IonContent, IonButtons, IonMenuButton, IonList, IonItem, IonIcon, menuController, IonThumbnail, IonCard, IonCardHeader, IonCardContent, IonButton, IonSplitPane } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { addIcons } from 'ionicons';
+import { arrowBack, arrowDown, layers, paperPlaneSharp, peopleSharp, newspaperSharp, walletSharp, arrowForwardCircle } from 'ionicons/icons';
+
+addIcons({
+  'arrow-back': arrowBack,
+  'arrow-down': arrowDown,
+  'layers': layers,
+  'paper-plane-sharp': paperPlaneSharp,
+  'people-sharp': peopleSharp,
+  'newspaper-sharp': newspaperSharp,
+  'wallet-sharp': walletSharp,
+  'arrow-forward-circle': arrowForwardCircle
+});
 
 
 export default defineComponent({
@@ -15,8 +28,11 @@ export default defineComponent({
     IonCardHeader, 
     IonCardContent, 
     IonButton,
+    IonSplitPane,
+    IonIcon,
     IonItem
   },
+  
   methods: {
     openFirst() {
       menuController.enable(true, 'first');
--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -2,13 +2,19 @@
   <ion-menu side="start" menu-id="first" content-id="main">
     <ion-header>
       <ion-toolbar>
-        <ion-title class="ion-text-center">Atena</ion-title>
+        <ion-title class="ion-text-center"> Atena </ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content>
       <ion-list lines="none">
-        <ion-item>Dashboard</ion-item>
-        <ion-item>Menu Item</ion-item>
+        <ion-item>
+          <ion-icon name="layers" />
+          Dashboard
+        </ion-item>
+        <ion-item>
+          <ion-icon name="arrow-down"></ion-icon>
+          Alvos
+        </ion-item>
         <ion-item>Menu Item</ion-item>
         <ion-item>Menu Item</ion-item>
         <ion-item>Menu Item</ion-item>
@@ -21,12 +27,7 @@
       <ion-buttons slot="start">
         <ion-menu-button></ion-menu-button>
       </ion-buttons>
-      <ion-title> Dashboard </ion-title>
-      <ion-buttons
-        class="buttonProfileDropDown"
-        slot="end"
-        @click="toggle()"
-      >
+      <ion-buttons class="buttonProfileDropDown" slot="end" @click="toggle()">
         <ion-thumbnail class="ion-margin-end">
           <img src="../../../public/assets/img/profile.png" alt="Avatar" />
         </ion-thumbnail>
@@ -47,10 +48,101 @@
 
       <ion-card-content>
         <div class="buttonDropdown">
-          <ion-button  color="primary">Perfil</ion-button>
-          <ion-button  color="danger">Sair</ion-button>
+          <ion-button color="primary">Perfil</ion-button>
+          <ion-button color="danger">Sair</ion-button>
         </div>
       </ion-card-content>
+    </ion-card>
+
+    <ion-card class="teste"> 
+      <ion-card-header class="card-container">
+        <ion-card-subtitle color="light">Dashboard</ion-card-subtitle>
+      </ion-card-header>
+      <ion-grid>
+        <ion-row class="ionRow">
+          <ion-col size="3">
+            <ion-card class="cardGrid" type="button" href="" target="_blank">
+              <div class="contentCard">
+                <div class="inner ion-margin-start">
+                  <h1>150</h1>
+                  <p>Alvos</p>
+                </div>
+                <div class="iconGrid">
+                  <ion-icon name="paper-plane-sharp"></ion-icon>
+                </div>
+              </div>
+              <a href="">
+                <div class="linkGrid">
+                  <div class="container-icon-grid">
+                    <ion-icon name="arrow-forward-circle"> </ion-icon>
+                  </div>
+                </div>
+              </a>
+            </ion-card>
+          </ion-col>
+          <ion-col size="3">
+            <ion-card class="cardGrid" type="button" href="" target="_blank">
+              <div class="contentCard">
+                <div class="inner ion-margin-start">
+                  <h1>150</h1>
+                  <p>Leads</p>
+                </div>
+                <div class="iconGrid">
+                  <ion-icon name="people-sharp"></ion-icon>
+                </div>
+              </div>
+              <a href="">
+                <div class="linkGrid">
+                  <div class="container-icon-grid">
+                    <ion-icon name="arrow-forward-circle"> </ion-icon>
+                  </div>
+                </div>
+              </a>
+            </ion-card>
+          </ion-col>
+          <ion-col size="3">
+            <ion-card class="cardGrid" type="button" href="" target="_blank">
+              <div class="contentCard">
+                <div class="inner ion-margin-start">
+                  <h1>150</h1>
+                  <p>Contratos abertos</p>
+                </div>
+                <div class="iconGrid">
+                  <ion-icon name="newspaper-sharp"></ion-icon>
+                </div>
+              </div>
+              <a href="">
+                <div class="linkGrid">
+                  <div class="container-icon-grid">
+                    <ion-icon name="arrow-forward-circle"> </ion-icon>
+                  </div>
+                </div>
+              </a>
+            </ion-card>
+          </ion-col>
+          <ion-col size="3">
+            <ion-card class="cardGrid" type="button" href="" target="_blank">
+              <div class="contentCard">
+                <div class="inner ion-margin-start">
+                  <h1>150</h1>
+                  <p>Comissões</p>
+                </div>
+                <div class="iconGrid">
+                  <ion-icon name="wallet-sharp"></ion-icon>
+                </div>
+              </div>
+              <a href="">
+                <div class="linkGrid">
+                  <div class="container-icon-grid">
+                    <ion-icon name="arrow-forward-circle"> </ion-icon>
+                  </div>
+                </div>
+              </a>
+            </ion-card>
+          </ion-col>
+          
+        </ion-row>
+      </ion-grid>
     </ion-card>
   </ion-content>
 
